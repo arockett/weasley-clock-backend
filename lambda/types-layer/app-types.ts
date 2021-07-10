d5e7d253bce75656a174f6e52d11fbc293311348
Aaron Beckett <aminor65ii@gmail.com>
2021-07-09 19:57:22 -0600

    Define Status enum in new types-layer for lambda functions

---
+++ lambda/types-layer/app-types.ts
@@ -0,0 +1,20 @@
+
+export enum Status {
+  /*
+   * Commented out status are on the roadmap ;)
+   */
+  Home,
+  InTransit,
+  Out,
+  //Abroad,
+  //Lost,
+
+  //Friends,
+  //Family,
+  //Work,
+  //Gym,
+  //HappyPlace,
+
+  //Airport,
+  //Hospital
+}
