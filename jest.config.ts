35d484db52d3fcbcb6109fd36b79dc7909682ae8
Aaron Beckett <aminor65ii@gmail.com>
2021-07-16 15:54:56 -0600

    Configure jest tests to automatically see module paths from tsconfig.json

--- jest.config.ts
+++ jest.config.ts
@@ -1,8 +1,13 @@
+import { pathsToModuleNameMapper } from 'ts-jest/utils';
+import * as tsconfig from './tsconfig.json';
+
 module.exports = {
   testEnvironment: 'node',
   roots: ['<rootDir>/test'],
   testMatch: ['**/*.test.ts'],
   transform: {
     '^.+\\.tsx?$': 'ts-jest'
-  }
+  },
+  modulePaths: ['<rootDir>'],
+  moduleNameMapper: pathsToModuleNameMapper(tsconfig.compilerOptions.paths)
 };
