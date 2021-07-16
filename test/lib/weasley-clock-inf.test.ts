c7ed1f6aff771c7cb3e01705ff5252cbb80b0fe5
Aaron Beckett <aminor65ii@gmail.com>
2021-07-16 15:44:50 -0600

    Organize jest tests into subdirs

--- test/lib/weasley-clock-inf.test.ts
+++ test/lib/weasley-clock-inf.test.ts
@@ -1,6 +1,6 @@
 import { expect as expectCDK, haveResource } from '@aws-cdk/assert';
 import * as cdk from '@aws-cdk/core';
-import { WeasleyClockInfStack } from '../lib/weasley-clock-inf-stack';
+import { WeasleyClockInfStack } from '../../lib/weasley-clock-inf-stack';
 
 test('Has Database', () => {
     const app = new cdk.App();
