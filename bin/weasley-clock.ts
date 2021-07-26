fb558ffc39b49b8a3b6c06f6d1ead2108be749c2
Aaron Beckett <aminor65ii@gmail.com>
2021-07-26 15:15:48 -0600

    Define simple CDKPipeline which builds project only

--- bin/weasley-clock.ts
+++ bin/weasley-clock.ts
@@ -1,8 +1,10 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
-import { WeasleyClock } from '../lib/weasley-clock';
+import { WeasleyClockPipeline } from '../lib/pipeline-stack';
 
 const app = new cdk.App();
 
-new WeasleyClock(app, 'WeasleyClock', {});
+new WeasleyClockPipeline(app, 'WeasleyClockPipeline', {
+  env: { account: '033318137562', region: 'us-east-2' }
+});
---
+++ lib/pipeline-stack.ts
@@ -0,0 +1,40 @@
+import * as cdk from '@aws-cdk/core';
+import { CdkPipeline, SimpleSynthAction } from '@aws-cdk/pipelines';
+
+import * as codepipeline from '@aws-cdk/aws-codepipeline';
+import * as codepipeline_actions from '@aws-cdk/aws-codepipeline-actions';
+
+
+export class WeasleyClockPipeline extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const sourceArtifact = new codepipeline.Artifact();
+    const cloudAssemblyArtifact = new codepipeline.Artifact();
+
+    const pipeline = new CdkPipeline(this, 'Pipeline', {
+      cdkCliVersion: '1.115.0',
+      cloudAssemblyArtifact: cloudAssemblyArtifact,
+
+      sourceAction: new codepipeline_actions.GitHubSourceAction({
+        actionName: 'GitHub',
+        output: sourceArtifact,
+        oauthToken: cdk.SecretValue.secretsManager('GITHUB_ACCESS_TOKEN'),
+        trigger: codepipeline_actions.GitHubTrigger.POLL,
+        // Replace these with your actual GitHub project info
+        owner: 'arockett',
+        repo: 'weasley-clock-backend',
+      }),
+
+      synthAction: SimpleSynthAction.standardNpmSynth({
+        sourceArtifact: sourceArtifact,
+        cloudAssemblyArtifact: cloudAssemblyArtifact,
+
+        // Use this if you need a build step (if you're not using ts-node
+        // or if you have TypeScript Lambdas that need to be compiled).
+        buildCommand: 'npm run build',
+        testCommands: ['npm run test']
+      }),
+    });
+  }
+}
